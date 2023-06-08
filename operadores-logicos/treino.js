1cd1fb6184015b6a67161e2473facb1271f7acf7
Alineopsoliveira <alinep730@gmail.com>
2023-06-07 22:29:20 -0300

    treinando o codigo

--- operadores-logicos/treino.js
+++ operadores-logicos/treino.js
@@ -1,34 +1,37 @@
-const readLine = require('readLine').createInterface({
+const readLine = require('readline').createInterface({ 
     input : process.stdin,
     output : process.stdout
-}); 
+}); /*readLine Ler input na linha de comando */
+/* require requisita */
 
-console.log("Esse programa vai verificar se a pessoa está na lista de convidados e se possui a idade permitida para entrar na festa")
-
-readLine.question('Qual o seu nome?', nome => {
-    switch(nome){
-        case 'Aline' :
-            console.log('Aline, voce esta na lista, vamos verificar sua idade');
-            break; 
-        case 'Ísis' :
-            console.log('Ísis, voce esta na lista, vamos verificar sua idade para liberar a entrada');
-            break;
-        case 'Luísa' :
-            console.log('Luísa, voce esta na lista, vamos verificar sua idade para liberar a entrada')
-            break;
-        case 'Italo' :
-            console.log('Italo, voce esta na lista, vamos verificar sua idade para liberar a entrada')
-        default:
-            console.log('Infelizmente seu nome não esta na lista');
-
-    readLine.question('Qual a sua idade?', idade =>{
-        if(idade < 18){
-            console.log('idade inferior a permitida, você não pode entrar')
-        }else{
-            console.log('idade permitida, aproveite a festa')
+readLine.question('Qual a sua idade? ', idade => {
+    if(idade < 18){
+        console.log('idade inferior a permitida, você não pode entrar')
+    }else{
+        
+        
+        console.log("Idade permitida, agora vamosverificar se seu nome está na lista");
+        readLine.question('Qual o seu nome? ', nome => {
+            switch(nome){
+                case 'Aline' :
+                    console.log('Aline, voce esta na lista, aproveite a festa!');
+                    break; 
+                case 'Ísis' :
+                    console.log('Ísis, voce esta na lista, aproveite a festa!');
+                    break;
+                case 'Luísa' :
+                    console.log('Luísa, voce esta na lista, aproveite a festa!')
+                    break;
+                case 'Italo' :
+                    console.log('Italo, voce esta na lista, aproveite a festa!')
+                    break;
+                default:
+                    console.log('Infelizmente seu nome não esta na lista');
+        
+        
+            }
         }
-    }
-)
+        )
     }
 }
 )
